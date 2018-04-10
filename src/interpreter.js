61b60516961a22f5785d1fb96fe6469617305c60
Victor Veytsman <vityavv@gmail.com>
2018-04-10 10:40:20 -0400

    Finish math expression parser!

--- src/interpreter.js
+++ src/interpreter.js
@@ -1,12 +1,18 @@
-//let vars = require("./vars.js");
-let vars = {};
-[vars.one, vars.two, vars.three, vars.four, vars.five] = [1,2,3,4,5];
-function interpretAndRun(lines) {
-}
-function parseMathExpression(expression) {
+function parseMathExpression(expression, vars) {
 	expression = expression.replace(/(?<=[\+\*\/\-\(\)])\s+/g, "");//remove all spaces before a (, ), +, -, *, or /
 	expression = expression.replace(/\s+(?=[\+\*\/\-\(\)])/g, "");//remove all spaces after a (, ), +, -, *, or /
 	expression = expression.replace(/[A-Za-z_]+/, variable => vars[variable]);//replace all variables with their values
+	while (expression.match(/(?<!\()\(.*?\)(?!\))/g)) {//work through each *single nested parentheses* and resolve it
+		expression = expression.replace(/(?<!\()\((.*?)\)(?!\))/g, (thing, exp) => {
+			return parseMathExpression(exp);
+		});
+	}
+	while (expression.match(/\*\*/g)) {//work through each exponentiation and resolve it
+		expression = expression.replace(/[\d.]+\*\*[\d.]+/, exp => {
+			let numbers = exp.split("**").map(Number);
+			return numbers[0] ** numbers[1];//ES7 ftw
+		});
+	}
 	while (expression.match(/\*|\//g)) {//work through each multiplication and division and resolve them
 		expression = expression.replace(/[\d.]+[\*\/][\d.]+/, exp => {
 			if (exp.match(/\//)) {
@@ -18,5 +24,17 @@ function parseMathExpression(expression) {
 			}
 		});
 	}
+	while (expression.match(/\+|-/g)) {//work through each addition and subtraction and resolve it
+		expression = expression.replace(/[\d.]+[\+\-][\d.]+/, exp => {
+			if (exp.match(/-/)) {
+				let numbers = exp.split("-").map(Number);
+				return numbers[0] - numbers[1];
+			} else {
+				let numbers = exp.split("+").map(Number);
+				return numbers[0] + numbers[1];
+			}
+		});
+	}
+	return Number(expression);
 }
 module.exports = parseMathExpression;
