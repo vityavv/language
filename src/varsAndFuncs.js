94f67af0c8ae5b1b6cb6bd2a62da0df399b555ad
Victor Veytsman <vityavv@gmail.com>
2018-04-13 13:19:35 -0400

    emergency commit. error handling for nowx

--- src/varsAndFuncs.js
+++ src/varsAndFuncs.js
@@ -3,11 +3,13 @@ let nativeAndVars = {
 	funcs: {
 		variable: function(variablename, value, ...args) {
 			if (args.length) throw Error(`Expected 2 argument for variable, got ${args.length + 2} or less`);
-			nativeAndVars.vars[variablename] = value;
+			if (variablename.type !== "string") throw Error(`Expected variablename to be a string, got a ${variablename.type} instead`);
+			if (value.type === "expression") value = parseMathExpression("expression");
+			nativeAndVars.vars[variablename.value] = value.value;
 		},
 		"var": this.variable,
 		print: function(...args) {
-			console.log(args.join("\n"));
+			if (){}
 		},
 	},
 	vars: {}
