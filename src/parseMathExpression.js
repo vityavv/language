bea178ef56af98ef3094f818d300fe0f55dc68b3
Victor Veytsman <vityavv@gmail.com>
2018-05-29 17:30:35 -0400

    Add support for negative numbers in expressions and do some more error handling

--- src/parseMathExpression.js
+++ src/parseMathExpression.js
@@ -12,26 +12,28 @@ function parseMathExpression(expression, vars = {}) {
 		throw new Error("Looks like you used a character that isn't allowed in a mathematical expression");
 	}
 	//error handling!
-	if (expression.match(/[\+\*\/\-\(\)%=!><]$|^[\+\*\/\-\(\)%=!><]/g)) {
+	if (expression.match(/[\+\*\/\-\(\)%=!><]$|^[\+\*\/\(\)%=!><]/g)) {
 		throw new Error("Looks like you ended your expression on a symbol, which is not allowed");
 	}
 	if (expression.match(/\s/g)) {
 		throw new Error("Looks like you have a stray space in your expression. I did my best to clean them out so look closely");
 	}
-
+	if (expression.match(/[\+\*\/\(\)&=!<>]{2,}|[\+\*\/\-\(\)%=!><]{3,}|-[\+\*\/\(\)&=!<>]/g)) {
+		throw new Error("Looks like you used more than two operators in a row, incorrectly");
+	}
 	while (expression.match(/(?<!\()\(.*?\)(?!\))/g)) {//work through each *single nested parentheses* and resolve it
 		expression = expression.replace(/(?<!\()\((.*?)\)(?!\))/g, (thing, exp) => {
 			return parseMathExpression(exp, vars);
 		});
 	}
 	while (expression.match(/\*\*/g)) {//work through each exponentiation and resolve it
-		expression = expression.replace(/[\d.]+\*\*[\d.]+/, exp => {
+		expression = expression.replace(/-?[\d.]+\*\*-?[\d.]+/, exp => {
 			let numbers = exp.split("**").map(Number);
 			return numbers[0] ** numbers[1];//ES7 ftw
 		});
 	}
 	while (expression.match(/[\*\/%]/g)) {//work through each multiplication and division and mod and resolve them
-		expression = expression.replace(/[\d.]+[\*\/%][\d.]+/, exp => {
+		expression = expression.replace(/-?[\d.]+[\*\/%]-?[\d.]+/, exp => {
 			if (exp.match(/\//)) {
 				let numbers = exp.split("/").map(Number);
 				return numbers[0] / numbers[1];
@@ -44,14 +46,14 @@ function parseMathExpression(expression, vars = {}) {
 			}
 		});
 	}
-	while (expression.match(/\+|-/g)) {//work through each addition and subtraction and resolve it
-		expression = expression.replace(/[\d.]+[\+\-][\d.]+/, exp => {
-			if (exp.match(/-/)) {
-				let numbers = exp.split("-").map(Number);
-				return numbers[0] - numbers[1];
-			} else {
+	while (expression.match(/\+|.-/g)) {//work through each addition and subtraction and resolve it
+		expression = expression.replace(/-?[\d.]+[\+\-]-?[\d.]+/, exp => {
+			if (exp.match(/\+/)) {
 				let numbers = exp.split("+").map(Number);
 				return numbers[0] + numbers[1];
+			} else {
+				let groups = /(-?[\d.]+)[\+\-](-?[\d.]+)/.exec(exp);
+				return Number(groups[1]) - Number(groups[2]);
 			}
 		});
 	}
